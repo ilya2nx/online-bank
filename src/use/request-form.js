fbd60ad2f6b5388c1fb8f201d2a7adda9e9cec81
ilya2nx <mishanin_95@mail.ru>
2021-04-30 13:42:24 +0300

    init

---
+++ src/use/request-form.js
@@ -0,0 +1,48 @@
+import { useField, useForm } from 'vee-validate'
+import * as yup from 'yup'
+
+export function useRequestForm() {
+  const {isSubmitting, handleSubmit} = useForm()
+
+  const {value: fio, errorMessage: fError, handleBlur: fBlur} = useField(
+    'fio', 
+    yup
+      .string()
+      .trim()
+      .required('Введите ФИО клиента')
+    )
+  const {value: phone, errorMessage: pError, handleBlur: pBlur} = useField(
+    'phone',
+    yup
+      .string()
+      .trim()
+      .required('Номер не может быть пустым')
+    )
+  const {value: amount, errorMessage: aError, handleBlur: aBlur} = useField(
+    'amount',
+    yup
+      .number()
+      .required('Введите сумму')
+      .min(0, 'Сумма не может быть меньше 0')
+    )
+  const {value: status} = useField('status')
+
+  const onSubmit = handleSubmit(async () => {
+
+  })
+
+  return {
+    status,
+    isSubmitting,
+    onSubmit,
+    fio,
+    fError,
+    fBlur,
+    phone,
+    amount,
+    pError,
+    pBlur,
+    aError,
+    aBlur
+  }
+}
